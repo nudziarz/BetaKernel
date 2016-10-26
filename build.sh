71ad33c5f1f1902ae3694fba4d847f1ba165db76
mrriddle <renata.kolek.czaniec@gmail.com>
2016-10-26 21:33:00 +0200

    modify creator script

--- build.sh
+++ build.sh
@@ -17,14 +17,12 @@ rm zip-files/tmp/kernels/925stock/boot.img
 echo "setting up eviorment"
 export ARCH=arm64
 export SUBARCH=arm64
-export KBUILD_BUILD_USER=patryq_dev
-export KBUILD_BUILD_HOST=ASUS
 export ANDROID_CCACHE_DIR="$pwd/.ccache"  
 export ANDROID_CCACHE_SIZE="50G" 
 export CROSS_COMPILE=/android/kernel/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 #make
 echo "make kernel"
-make ARCH=arm64 exynos7420-zerolte_defconfig
+make ARCH=arm64 patryq_defconfig
 #make menuconfig
 make ARCH=arm64
 #copy_files
