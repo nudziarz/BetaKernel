1e8208854b41a390da6c5855b0caa863b4c2272a
mrriddle <renata.kolek.czaniec@gmail.com>
2016-10-26 16:42:08 +0200

    add CCACHE

--- build_kernel_test.sh
+++ build_kernel_test.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 #script by PatryQ-DEV
-
+#export 
 export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER=patryq_dev
+export KBUILD_BUILD_HOST=ASUS
+export ANDROID_CCACHE_DIR="$pwd/.ccache"  
+export ANDROID_CCACHE_SIZE="50G" 
 export CROSS_COMPILE=/android/kernel/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-
+#make
 make ARCH=arm64 exynos7420-zerolte_defconfig
 make menuconfig
 make ARCH=arm64
+#copy_files
+
+
+#zip_files
